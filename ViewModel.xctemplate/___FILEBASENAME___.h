2eba02d61e9b27a15a9efa4cf220cf510b30771c
Sergii Iastremskyi <s.iastremskyi@wnb.com.ua>
2020-05-11 22:24:44 +0300

    Added single ViewModel template

---
+++ ViewModel.xctemplate/___FILEBASENAME___.h
@@ -0,0 +1,17 @@
+//___FILEHEADER___
+
+@class ___VARIABLE_interactorType:identifier___;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : NSObject
+
+- (instancetype)initWithInteractor:(___VARIABLE_interactorType:identifier___ *)interactor;
+- (instancetype)init NS_UNAVAILABLE;
+
+@property (nonatomic) RACChannel *loading;
+@property (nonatomic) RACSubject *errors;
+
+@end
+
+NS_ASSUME_NONNULL_END
